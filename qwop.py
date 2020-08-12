6ec90e60e79c15ba92408f44239705a3fa202843
alinen <nenila@gmail.com>
2020-08-12 18:13:11 +0100

    add head

--- qwop.py
+++ qwop.py
@@ -119,6 +119,19 @@ def hit_ground(arbiter, space, data):
     print("hit ground!")
     return True
 
+def create_joint(space, b1, b2, px, py, lim1, lim2):
+    """
+    b1, b2: Body objects
+    px, py: (float) Pivot point in world coordinates
+    lim1, lim2: (float)  joint rotation limits (radians)
+    """
+    b1_b2 = pymunk.PivotJoint(b1, b2, (px, py))
+    b1_b2.collide_bodies = False
+    space.add(b1_b2)
+
+    b1_b2_limit = pymunk.RotaryLimitJoint(b1, b2, -math.pi/10, math.pi/10)
+    space.add(b1_b2_limit)
+
 def setup_character(space):
 
     handler = space.add_collision_handler(100, 1)
@@ -130,15 +143,16 @@ def setup_character(space):
     floor.collision_type = 100
     space.add(floor)
 
-    mass = 10
+    mass = 20
     h = 200
     w = 100
     bodyx = window.width // 2
-    bodyy = floorHeight + h + h/2 + 10 # 10 = margin
+    bodyy = floorHeight + h + h/8 + 10 # 10 = margin
 
     global thighL, thighR, calfL, calfR
 
     torso = setup_body(space, bodyx+0, bodyy+h*3/8, mass*2, w, h*3/4, 1, 2)
+    head = setup_body(space, bodyx+0, bodyy+h*7/8, mass/2.0, w/2, w, 1, 2)
 
     thighL = setup_body(space, bodyx-w/4.0, bodyy-h/4.0, mass, w/2.0, h/2.0, 2)
     thighR = setup_body(space, bodyx+w/4.0, bodyy-h/4.0, mass, w/2.0, h/2.0, 2)
@@ -149,53 +163,26 @@ def setup_character(space):
     footL = setup_body(space, bodyx-w/4+w/8, bodyy-h*17/16, mass/2.0, w*3/4, h/8, 2)
     footR = setup_body(space, bodyx+w/4+w/8, bodyy-h*17/16, mass/2.0, w*3/4, h/8, 2)
 
-    torso_thighL = pymunk.PivotJoint(torso, thighL, (bodyx-w/4, bodyy))
-    torso_thighR = pymunk.PivotJoint(torso, thighR, (bodyx+w/4, bodyy))
-    thighL_calfL = pymunk.PivotJoint(thighL, calfL, (bodyx-w/4.0, bodyy-h/2.0))
-    thighR_calfR = pymunk.PivotJoint(thighR, calfR, (bodyx+w/4.0, bodyy-h/2.0))
-    calfL_footL = pymunk.PivotJoint(calfL, footL, (bodyx-w/4.0, bodyy-h))
-    calfR_footR = pymunk.PivotJoint(calfR, footR, (bodyx+w/4.0, bodyy-h))
+    create_joint(space, torso,  head,   bodyx,       bodyy+h*3/4, -math.pi/10,  math.pi/10)
+    create_joint(space, torso,  thighL, bodyx-w/4,   bodyy      , -math.pi*3/4, math.pi/2)
+    create_joint(space, torso,  thighR, bodyx+w/4,   bodyy      , -math.pi*3/4, math.pi/2)
+    create_joint(space, thighL, calfL,  bodyx-w/4.0, bodyy-h/2.0, -math.pi/2,  -math.pi/10 )
+    create_joint(space, thighR, calfR,  bodyx+w/4.0, bodyy-h/2.0, -math.pi/2,  -math.pi/10)
+    create_joint(space, calfL,  footL,  bodyx-w/4.0, bodyy-h    , -math.pi/10,  math.pi/10)
+    create_joint(space, calfR,  footR,  bodyx+w/4.0, bodyy-h    , -math.pi/10,  math.pi/10)
 
-    torso_thighL.collide_bodies = False
-    torso_thighR.collide_bodies = False
-    thighL_calfL.collide_bodies = False
-    thighR_calfR.collide_bodies = False
-    calfL_footL.collide_bodies = False
-    calfR_footR.collide_bodies = False
+    ## For debugging
+    ##torso_pin = pymunk.PinJoint(torso, space.static_body, (0,h*3/8), (bodyx, bodyy))
+    ##space.add(torso_pin)
 
-    # For debugging
-    #torso_pin = pymunk.PinJoint(torso, space.static_body, (0,h*3/8), (bodyx, bodyy))
-    #space.add(torso_pin)
-
-    space.add(torso_thighL)
-    space.add(torso_thighR)
-    space.add(thighL_calfL)
-    space.add(thighR_calfR)
-    space.add(calfL_footL)
-    space.add(calfR_footR)
-
-    torso_thighL_limit = pymunk.RotaryLimitJoint(torso, thighL, -math.pi*3/4, math.pi/2)
-    torso_thighR_limit = pymunk.RotaryLimitJoint(torso, thighR, -math.pi*3/4, math.pi/2)
-    thighL_calfL_limit = pymunk.RotaryLimitJoint(thighL, calfL, -math.pi/2, -math.pi/10)
-    thighR_calfR_limit = pymunk.RotaryLimitJoint(thighR, calfR, -math.pi/2, -math.pi/10)
-    calfL_footL_limit = pymunk.RotaryLimitJoint(calfL, footL, -math.pi/10, math.pi/10)
-    calfR_footR_limit = pymunk.RotaryLimitJoint(calfR, footR, -math.pi/10, math.pi/10)
-
-    space.add(torso_thighL_limit)
-    space.add(torso_thighR_limit)
-    space.add(thighL_calfL_limit)
-    space.add(thighR_calfR_limit)
-    space.add(calfL_footL_limit)
-    space.add(calfR_footR_limit)
-
-    angle = -math.pi/6
-    rx = 0
-    ry = thighL.height / 2
-    off_x = math.cos(angle) * rx - math.sin(angle) * ry
-    off_y = math.sin(angle) * rx + math.cos(angle) * ry
-    thighL.position = (thighL.position[0] + rx - off_x, thighL.position[1] + ry - off_y)
-    print(rx, ry, off_x, off_y, thighL.position)
-    thighL.angle = angle
+    #angle = -math.pi/6
+    #rx = 0
+    #ry = thighL.height / 2
+    #off_x = math.cos(angle) * rx - math.sin(angle) * ry
+    #off_y = math.sin(angle) * rx + math.cos(angle) * ry
+    #thighL.position = (thighL.position[0] + rx - off_x, thighL.position[1] + ry - off_y)
+    #print(rx, ry, off_x, off_y, thighL.position)
+    #thighL.angle = angle
 
     #calfL.angle = -math.pi/4
 
